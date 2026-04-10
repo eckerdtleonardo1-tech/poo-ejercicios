9fc466cba671282c874413326bf87e604861a13c
Leonardo <eckerdtleonardo1@gmail.com>
2026-04-09 22:07:45 -0300

    funcion acelerar auto

--- auto.js
+++ auto.js
@@ -2,9 +2,19 @@ class Auto {
     constructor(marca, modelo) {
         this.marca = marca;
         this.modelo = modelo;
+        this.velocidad = 0;
     }
+
+    acelerar(){
+        this.velocidad += 10;
+    }
+
 }
 
 
 const auto1 = new Auto("Ford", "Fiesta");
 const auto2 = new Auto("Toyota", "Corolla");
+const auto3 = new Auto("toyota", "etios");
+
+auto1.acelerar();
+console.log("La velocidad del coche 1 es " + auto1.velocidad);
